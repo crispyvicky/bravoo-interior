0ed6b519797cec80a102ffc8a759c4e45b980520
leadwithvicky <vicky@visiontech.world>
2025-12-26 19:10:43 +0530

    ok

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'dist',
+    output: 'export',
     images: {
         unoptimized: true, // Crucial for using existing <img> tags without errors
     },
